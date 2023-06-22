7c5a824056ed48828d229e1d0b43e27f5ab2810b
Iv Po <me@dotdev.ru>
2023-06-23 00:41:41 +0300

    (crud) fixes

--- pkg/crud/crud.go
+++ pkg/crud/crud.go
@@ -1,8 +1,6 @@
 package crud
 
 import (
-	"database/sql"
-
 	"dotdev/nest/pkg/nest"
 
 	"gorm.io/gorm"
@@ -10,7 +8,7 @@ import (
 
 type Crud[T IModel] struct {
 	db *gorm.DB
-	tx *gorm.DB
+	// tx *gorm.DB
 }
 
 // NewService godoc
@@ -33,15 +31,17 @@ func (s *Crud[T]) IsValid(ctx nest.Context, input interface{}) error {
 }
 
 // Flush godoc
-func (s *Crud[T]) Flush(data T) error {
-	if data.GetPk() > 0 || data.GetID() != "" {
-		return s.Tx().Save(data).Error
-	}
+// func (s *Crud[T]) Flush(data T) error {
+// 	if data.GetPk() > 0 || data.GetID() != "" {
+// 		return s.Tx().Save(data).Error
+// 	}
 
-	return s.Tx().Create(data).Error
-}
+// 	return s.Tx().Create(data).Error
+// }
 
 // Find godoc
+// @deprecated
+// TODO: move to repository
 func (s *Crud[T]) Find(result T, id interface{}, options ...Option) error {
 	stmt := s.Stmt(options...)
 
@@ -49,6 +49,8 @@ func (s *Crud[T]) Find(result T, id interface{}, options ...Option) error {
 }
 
 // FindAll godoc
+// @deprecated
+// TODO: move to repository
 func (s *Crud[T]) FindAll(result interface{}, options ...Option) error {
 	var stmt = s.Stmt(options...)
 
@@ -57,7 +59,7 @@ func (s *Crud[T]) FindAll(result interface{}, options ...Option) error {
 
 // Stmt godoc
 func (s *Crud[T]) Stmt(options ...Option) *gorm.DB {
-	var stmt = s.Tx().Session(&gorm.Session{}) //NewDB: true
+	var stmt = s.db.Session(&gorm.Session{}) // Tx() //NewDB: true
 
 	for _, option := range options {
 		stmt = option(stmt)
@@ -66,63 +68,69 @@ func (s *Crud[T]) Stmt(options ...Option) *gorm.DB {
 	return stmt
 }
 
-// DB Godoc
-func (s *Crud[T]) DB() *gorm.DB {
-	return s.db
-}
-
 // Tx Get current transaction
-func (s *Crud[T]) Tx() *gorm.DB {
-	if s.tx != nil {
-		return s.tx
-	}
-	return s.db
-}
+//
+// TODO: @internal remove refactor
+// func (s *Crud[T]) Tx() *gorm.DB {
+// 	// if s.tx != nil {
+// 	// 	return s.tx
+// 	// }
+// 	return s.db
+// }
+
+// DB Godoc
+// @deprecated
+// TODO: remove/refactor
+// func (s *Crud[T]) DB() *gorm.DB {
+// 	return s.db
+// }
 
 // Begin godoc
-func (s *Crud[T]) Begin(opts ...*sql.TxOptions) (*gorm.DB, error) {
-	s.tx = s.db.Begin(opts...)
+// func (s *Crud[T]) Begin(opts ...*sql.TxOptions) (*gorm.DB, error) {
+// 	s.tx = s.db.Begin(opts...)
 
-	defer func() {
-		if r := recover(); r != nil && s.tx != nil {
-			s.tx.Rollback()
-		}
-	}()
+// 	defer func() {
+// 		if r := recover(); r != nil && s.tx != nil {
+// 			s.tx.Rollback()
+// 		}
+// 	}()
 
-	if err := s.tx.Error; err != nil {
-		return nil, err
-	}
+// 	if err := s.tx.Error; err != nil {
+// 		return nil, err
+// 	}
 
-	return s.tx, nil
-}
+// 	return s.tx, nil
+// }
 
 // Commit godoc
-func (s *Crud[T]) Commit() *gorm.DB {
-	if s.tx == nil {
-		return nil
-	}
+// func (s *Crud[T]) Commit() *gorm.DB {
+// 	if s.tx == nil {
+// 		return nil
+// 	}
 
-	tx := s.tx.Commit()
-	s.tx = nil
-	return tx
-}
+// 	tx := s.tx.Commit()
+
+// 	s.tx = nil
+
+// 	return tx
+// }
 
 // Rollback godoc
-func (s *Crud[T]) Rollback() {
-	if s.tx == nil {
-		return
-	}
+// func (s *Crud[T]) Rollback() {
+// 	if s.tx == nil {
+// 		return
+// 	}
 
-	s.tx.Rollback()
-	s.tx = nil
-}
+// 	s.tx.Rollback()
+// 	s.tx = nil
+// }
 
 // Transaction godoc
-func (s *Crud[T]) Transaction(fc func(tx *gorm.DB) (err error), opts ...*sql.TxOptions) error {
-	return s.db.Transaction(func(tx *gorm.DB) error {
-		s.tx = tx
-		err := fc(tx)
-		s.tx = nil
-		return err
-	}, opts...)
-}
+// func (s *Crud[T]) Transaction(fc func(tx *gorm.DB) (err error), opts ...*sql.TxOptions) error {
+// 	return s.db.Transaction(func(tx *gorm.DB) error {
+// 		s.tx = tx
+// 		err := fc(tx)
+// 		s.tx = nil
+// 		return err
+// 	}, opts...)
+// }
