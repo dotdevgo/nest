1d927397ac1aa1187b5d831c7693778f6160e41c
Iv Po <me@dotdev.ru>
2023-11-04 16:16:35 +0200

    swagger: init

---
+++ pkg/swagger/swagger.go
@@ -0,0 +1,19 @@
+package swagger
+
+import (
+	"dotdev/nest/pkg/nest"
+
+	"github.com/defval/di"
+	swag "github.com/swaggo/echo-swagger"
+)
+
+// New godoc
+func New() di.Option {
+	return di.Options(
+		di.Invoke(func(e *nest.Kernel) error {
+			e.Echo.GET("/docs/*", swag.WrapHandler)
+
+			return nil
+		}),
+	)
+}
